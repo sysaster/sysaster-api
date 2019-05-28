402aad9c89ebc4fccab352bd061bd9932bbd87bb
Vitor Greati <greati@ufrn.edu.br>
2019-05-28 14:26:04 -0300

    Receive cropeed

--- sysaster-api.py
+++ sysaster-api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import cv2
 import numpy as np
 import base64
+import random
 
 app = Flask(__name__)
 
@@ -13,7 +14,13 @@ def save_person_detection_data():
         nparr = np.fromstring(img_data, dtype=np.uint8)
         w = json_data["bounding_box"]["w"]
         h = json_data["bounding_box"]["h"]
-        nparr = nparr.reshape(int(h), int(w))
-        cv2.imwrite("clipped.png", nparr);
+        c = json_data["bounding_box"]["c"]
+        nparr = nparr.reshape(int(c) * int(h), int(w))
+        b = nparr[0:int(h),:]
+        g = nparr[int(h):2*int(h),:]
+        r = nparr[2*int(h):3*int(h),:]
+        img = cv2.merge((b, g, r))
+        img_rand_id = random.random()*100
+        cv2.imwrite("clipped_"+str(img_rand_id)+".png", img);
         return "abc"
 
